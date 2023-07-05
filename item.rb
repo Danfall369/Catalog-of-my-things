ca89189cfe295a2066608fd79caaa6aa36c7c8f7
Daniel Falcon <Danfall963@Gmail.com>
2023-07-05 15:57:30 -0400

    Adding item.rb and main.rb

---
+++ item.rb
@@ -0,0 +1,28 @@
+require 'date'
+
+class Item
+  attr_reader :id, :genre, :author, :label, :publish_date
+  attr_accessor :archived
+
+  def initialize(id, genre, author, label, publish_date)
+    @id = id
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def can_be_archived?
+    (Time.now.year - publish_date.year) > 10
+  end
+
+  def move_to_archived
+    if can_be_archived?
+      @archived = true
+      puts 'Item moved to archived.'
+    else
+      puts 'Item cannot be archived.'
+    end
+  end
+end
