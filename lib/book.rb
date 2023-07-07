9411c034f0937b87cadf24c4a1d42efeae8d9b87
Daniel Falcon <Danfall963@Gmail.com>
2023-07-06 23:59:54 -0400

    Data  preservation in json files

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 require_relative 'item'
+require 'json'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
@@ -10,7 +11,7 @@ class Book < Item
   end
 
   def can_be_archived?
-    super || @cover_state == "bad"
+    super || @cover_state == 'bad'
   end
 
   def move_to_archived
@@ -20,5 +21,38 @@ class Book < Item
     else
       puts 'Item cannot be archived.'
     end
+  end
+
+  def save
+    book_data = {
+      id: id,
+      genre: genre,
+      author: author,
+      label: label,
+      publish_date: publish_date.to_s,
+      publisher: publisher,
+      cover_state: cover_state,
+      archived: archived
+    }
+
+    books = []
+
+    if File.exist?(file_path)
+      # Si el archivo existe, se lee su contenido actual
+      existing_data = File.read(file_path)
+      books = JSON.parse(existing_data) unless existing_data.empty?
+    end
+
+    books << book_data  # Se agrega el nuevo libro al arreglo de libros
+
+    File.open(file_path, 'w') do |file|
+      file.write(JSON.generate(books))
+    end
+  end
+
+  private
+
+  def file_path
+    './data/book.json'
   end
 end
