2f10d3d7bcd5bcfd1767406d617fa31de95e2ae8
Daniel Falcon <Danfall963@Gmail.com>
2023-07-07 01:26:22 -0400

    update boo spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/book'
 require 'date'
 require 'rspec'
+require 'json'
 
 describe Book do
   let(:publish_date) { Date.new(2010, 1, 1) }
@@ -55,6 +56,54 @@ describe Book do
       it 'prints "Item cannot be archived."' do
         expect { book.move_to_archived }.to output("Item cannot be archived.\n").to_stdout
       end
+    end
+  end
+
+  describe '#save' do
+    let(:file_path) { './data/book.json' }
+    let(:existing_data) { '' }
+    let(:file) { double('File') }
+
+    before do
+      allow(File).to receive(:exist?).with(file_path).and_return(true)
+      allow(File).to receive(:read).with(file_path).and_return(existing_data)
+      allow(File).to receive(:open).with(file_path, 'w').and_yield(file)
+      allow(file).to receive(:write)
+    end
+
+    context 'when the file is empty' do
+      it 'writes the book data to the file' do
+        book.save
+        expect(file).to have_received(:write).with(JSON.generate([book_data]))
+        puts "Book data saved successfully!"
+      end
+    end
+
+    context 'when the file is not empty' do
+      let(:existing_data) { '[{"id": 2}]' }
+
+      it 'appends the book data to the file' do
+        book.save
+        expect(file).to have_received(:write).with(JSON.generate(existing_books << book_data))
+        puts "Book data appended to the file successfully!"
+      end
+    end
+
+    def book_data
+      {
+        id: 1,
+        genre: 'Genre',
+        author: 'Author',
+        label: 'Label',
+        publish_date: publish_date.to_s,
+        publisher: 'Publisher',
+        cover_state: 'good',
+        archived: false
+      }
+    end
+
+    def existing_books
+      JSON.parse(existing_data)
     end
   end
 end
