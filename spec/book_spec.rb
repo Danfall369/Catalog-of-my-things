a8b212fd9f647089e4b0cc9d5573f0d8e7208364
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-07-08 09:04:03 -0500

    Unit testing
    - added rspec to run unit tests in project
    - corrected some of the UI basic style
    - checked linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,11 @@
-require_relative '../item'
-require_relative '../methods/book'
+require './item'
+require './methods/book'
 require 'date'
 
 describe Book do
-  let(:publish_date) { Date.new(2010, 1, 1) }
+  let(:publish_date) { Date.new(2015, 0o4, 16) }
   let(:publisher) { 'Example Publisher' }
-  let(:cover_state) { 'Good' }
+  let(:cover_state) { 'Bad' }
 
   subject(:book) { described_class.new(publish_date, publisher, cover_state) }
 
@@ -13,7 +13,7 @@ describe Book do
     it 'sets the publish_date, publisher, and cover_state' do
       expect(book.publish_date).to eq(publish_date)
       expect(book.publisher).to eq(publisher)
-      expect(book.cover_state).to eq('Good')
+      expect(book.cover_state).to eq('Bad')
     end
   end
 
@@ -27,7 +27,7 @@ describe Book do
     end
 
     context 'when publish_date is more than 10 years ago' do
-      let(:publish_date) { Date.new(Time.now.year - 11, 1, 1) }
+      let(:publish_date) { Date.new(Time.now.year - 12, 3, 4) }
 
       it 'returns true' do
         expect(book.can_be_archived?).to be true
@@ -35,7 +35,7 @@ describe Book do
     end
 
     context 'when publish_date is less than 10 years ago' do
-      let(:publish_date) { Date.new(Time.now.year - 9, 1, 1) }
+      let(:publish_date) { Date.new(Time.now.year - 8, 4, 2) }
 
       it 'returns false' do
         expect(book.can_be_archived?).to be false
