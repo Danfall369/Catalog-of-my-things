69a47e5fbc093670b2aaceff04c43b6237b05fde
Ellyboi <vitoesitemitope@gmail.com>
2023-07-08 22:43:01 +0100

    add game files

--- main.rb
+++ main.rb
@@ -18,12 +18,14 @@ class Main
       puts ''
       puts '1. List all books'
       puts '2. List all music albums'
-      puts '3. List all genres'
-      puts '4. List all labels'
-      puts '5. List all authors'
-      puts '6. Add a book'
-      puts '7. Add a music album'
-      puts '8. Exit'
+      puts '3. List all games'
+      puts '4. List all genres'
+      puts '5. List all labels'
+      puts '6. List all authors'
+      puts '7. Add a book'
+      puts '8. Add a music album'
+      puts '9. Add a game'
+      puts '10. Exit'
       puts
       user_choice = gets.chomp
       option_selected(user_choice)
@@ -34,12 +36,14 @@ class Main
     options = {
       '1' => :list_books,
       '2' => :list_music_albums,
-      '3' => :list_genres,
-      '4' => :list_labels,
-      '5' => :list_authors,
-      '6' => :add_book,
-      '7' => :add_music_album,
-      '8' => :quit
+      '3' => :list_games,
+      '4' => :list_genres,
+      '5' => :list_labels,
+      '6' => :list_authors,
+      '7' => :add_book,
+      '8' => :add_music_album,
+      '9' => :add_game,
+      '10' => :quit
     }
 
     method = options[user_choice]
